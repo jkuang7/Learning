4a41260b0e03fd3f220dfe98b8885efe7aab44e4
Jian <jiankuang7@gmail.com>
2022-05-02 14:59:38 -0400

    Adding web scraping

---
+++ Web Scraping in Python With BeautifulSoup and Selenium/test.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup
+import lxml
+import requests
+
+# Basics of BeautifulSoup
+# Acquiring HTML of web page via Python
+# Components -- Tag, NavigableString, Strings, Attributes, Comments
+
