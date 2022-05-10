bc37daaa559b8a209e6ccb326ba76d9c9ef3b850
Jian <jiankuang7@gmail.com>
2022-05-10 01:09:20 -0400

    Pushing IMDB.py

---
+++ 9. Exercise: IMDB.py
@@ -0,0 +1,4 @@
+# Open in google chrome -> top 100 movies of all time
+# https://www.imdb.com/chart/top
+# create a waittime -> self-scroll all the way down to 50th best movie of all time
+# click on the movie -> grab the image
